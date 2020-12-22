734cc52751b8fd2ac1e466b652502d5c710a1d89
Admin <mjrahey@mun.ca>
2020-12-22 01:54:37 -0330

    added missing files

---
+++ src/data/jobsData.js
@@ -0,0 +1,22 @@
+export const jobsData = [
+{
+	experienceImage: "data/images/nokia-logo.jpg",
+	companyName: "NOKIA" ,
+	jobTitle: "Software Developer Coop",
+	dateRange: "2020-2020",
+	jobDescription: "this is what I do",
+},
+{
+	experienceImage: "nokiaLogo",
+	companyName: "totaliQ" ,
+	jobTitle: "Fullstack Developer",
+	dateRange: "2020-2020",
+	jobDescription: "this is what I do",
+},
+{
+	experienceImage: "nokiaLogo",
+	companyName: "Protocase" ,
+	jobTitle: "nokia:)",
+	dateRange: "2020-2020",
+	jobDescription: "this is what I do",
+}]
