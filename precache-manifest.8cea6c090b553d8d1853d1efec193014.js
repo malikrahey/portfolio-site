878c7523ce5dcdc2c27ab47190149f19691ce84a
Admin <mjrahey@mun.ca>
2020-12-29 00:42:05 -0330

    Updates

---
+++ precache-manifest.8cea6c090b553d8d1853d1efec193014.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9744e707e3e183f6980d3dae303b7a69",
+    "url": "/portfolio-site/index.html"
+  },
+  {
+    "revision": "f052c670d0fcb24265dc",
+    "url": "/portfolio-site/static/css/main.9f2653b7.chunk.css"
+  },
+  {
+    "revision": "0c62cfed5d16cda37f10",
+    "url": "/portfolio-site/static/js/2.7f60609f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio-site/static/js/2.7f60609f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f052c670d0fcb24265dc",
+    "url": "/portfolio-site/static/js/main.f35d01d3.chunk.js"
+  },
+  {
+    "revision": "a2a773adb23ff47de08a",
+    "url": "/portfolio-site/static/js/runtime-main.2d95e7a6.js"
+  },
+  {
+    "revision": "e7b8fcd4ceb430ffc60ed55e5ab98fa5",
+    "url": "/portfolio-site/static/media/cs-logo.e7b8fcd4.png"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/portfolio-site/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "b37503581bc1cc8711ef17997a6ea30f",
+    "url": "/portfolio-site/static/media/nokia-logo.b3750358.png"
+  },
+  {
+    "revision": "fd6984368ba75ec90b8f9fe0cae05566",
+    "url": "/portfolio-site/static/media/python-logo.fd698436.png"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/portfolio-site/static/media/react-logo.ee7cd8ed.svg"
+  }
+]);
