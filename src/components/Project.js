08d4365c943618febbf84801b10fe1eca9cbce2e
Admin <mjrahey@mun.ca>
2021-01-02 00:41:02 -0330

    project cards now expandable

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,6 +3,7 @@ import './Project.css';
 import pythonLogo from '../data/images/python-logo.png';
 import csLogo from '../data/images/cs-logo.png';
 import reactLogo from '../data/images/react-logo.svg';
+import { Fade } from 'react-reveal';
 
 
 const LOGO_MAP = {
@@ -18,16 +19,25 @@ export default class Project extends React.Component {
     constructor(props) {
         super(props);
 
+        this.state = {
+            isExpanded: false
+        }
+
     }
 
     render() {
         return(
-            <div className="project">
+            <div className="project" onClick={() => {this.setState({isExpanded: !this.state.isExpanded})}}>
                 <div>
                 <img className="project-icon" src={LOGO_MAP[this.props.icon]} alt="icon"/>
                 </div>
                 <div className="project-title">{this.props.title}</div>
-                <div className="project-description">{this.props.description}</div>
+                
+                    {this.state.isExpanded ? 
+                    <Fade><div className="project-description">{this.props.description}</div> </Fade>
+                    : 
+                    <></>
+                    }
             </div>
         );
     }
